7a6cc6ca27ebc59fb25623068c2a69eb6b1a0f81
Vlit88 <valerylit88@gmail.com>
2023-01-04 22:28:20 +0300

    feat: add top-slider

--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -38,3 +38,23 @@ export function headerFixed() {
     headerStickyObserver.observe(firstScreen);
   }
 }
+
+// top slider //
+let dots = 4;
+let sliderElem = document.querySelector(".slider");
+let dotElems = sliderElem.querySelectorAll(".slider__dot");
+let indicatorElem = sliderElem.querySelector(".slider__indicator");
+
+Array.prototype.forEach.call(dotElems, (dotElem) => {
+  dotElem.addEventListener("click", (e) => {
+    let currentPos = parseInt(sliderElem.getAttribute("data-pos"));
+    let newPos = parseInt(dotElem.getAttribute("data-pos"));
+
+    let newDirection = newPos > currentPos ? "right" : "left";
+    let currentDirection = newPos < currentPos ? "right" : "left";
+
+    indicatorElem.classList.remove(`slider__indicator--${currentDirection}`);
+    indicatorElem.classList.add(`slider__indicator--${newDirection}`);
+    sliderElem.setAttribute("data-pos", newPos);
+  });
+});
