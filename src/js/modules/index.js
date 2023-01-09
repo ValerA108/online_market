6fa58d2eac4191fae7a12e8a4eb1d6e06989c5de
Vlit88 <valerylit88@gmail.com>
2023-01-09 19:04:06 +0300

    fix: rework all top-slider

--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -40,21 +40,250 @@ export function headerFixed() {
 }
 
 // top slider //
-let dots = 4;
-let sliderElem = document.querySelector(".slider");
-let dotElems = sliderElem.querySelectorAll(".slider__dot");
-let indicatorElem = sliderElem.querySelector(".slider__indicator");
 
-Array.prototype.forEach.call(dotElems, (dotElem) => {
-  dotElem.addEventListener("click", (e) => {
-    let currentPos = parseInt(sliderElem.getAttribute("data-pos"));
-    let newPos = parseInt(dotElem.getAttribute("data-pos"));
+const sliders = document.querySelectorAll(".slider");
+// interval between switching images
+// can't be less than your animation duration in css!
+const interval = 3800;
+// if you don't want to first animation last longer than other animations
+// set animDuration (in miliseconds) to your value of animation duration in css
+const animDuration = 600;
 
-    let newDirection = newPos > currentPos ? "right" : "left";
-    let currentDirection = newPos < currentPos ? "right" : "left";
+for (let i = 0; i < sliders.length; ++i) {
+  const slider = sliders[i];
+  const dots = slider.querySelector(".dots");
+  const sliderImgs = slider.querySelectorAll(".img");
 
-    indicatorElem.classList.remove(`slider__indicator--${currentDirection}`);
-    indicatorElem.classList.add(`slider__indicator--${newDirection}`);
-    sliderElem.setAttribute("data-pos", newPos);
-  });
-});
+  let currImg = 0;
+  let prevImg = sliderImgs.length - 1;
+  let intrvl;
+  let timeout;
+
+  // Creates dots and add listeners to them
+  for (let i = 0; i < sliderImgs.length; ++i) {
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+    dots.appendChild(dot);
+    dot.addEventListener("click", dotClick.bind(null, i), false);
+  }
+
+  const allDots = dots.querySelectorAll(".dot");
+  allDots[0].classList.add("active-dot");
+
+  sliderImgs[0].style.left = "0";
+  timeout = setTimeout(() => {
+    animateSlider();
+    sliderImgs[0].style.left = "";
+    intrvl = setInterval(animateSlider, interval);
+  }, interval - animDuration);
+
+  /**
+   * Animates images
+   * @param {number} [nextImg] - index of next image to show
+   * @param {boolean} [right = false] - animate to right
+   */
+
+  let nextImg;
+
+  function animateSlider(nextImg, right) {
+    if (!nextImg) nextImg = currImg + 1 < sliderImgs.length ? currImg + 2 : 1;
+
+    --nextImg;
+    sliderImgs[prevImg].style.animationName = "";
+
+    if (!right) {
+      sliderImgs[nextImg].style.animationName = "leftNext";
+      sliderImgs[currImg].style.animationName = "leftCurr";
+    } else {
+      sliderImgs[nextImg].style.animationName = "rightNext";
+      sliderImgs[currImg].style.animationName = "rightCurr";
+    }
+
+    prevImg = currImg;
+    currImg = nextImg;
+
+    let currDot = allDots[currImg];
+    currDot.classList.add("active-dot");
+    let prevDot = allDots[prevImg];
+    prevDot.classList.remove("active-dot");
+  }
+
+  function rightSlider(nextImg) {
+    if (currImg <= 0) {
+      nextImg = sliderImgs.length - 1;
+    } else {
+      nextImg = currImg - 1;
+    }
+
+    sliderImgs[prevImg].style.animationName = "";
+
+    sliderImgs[nextImg].style.animationName = "rightNext";
+    sliderImgs[currImg].style.animationName = "rightCurr";
+
+    prevImg = currImg;
+    currImg = nextImg;
+
+    let currDot = allDots[currImg];
+    currDot.classList.add("active-dot");
+    let prevDot = allDots[prevImg];
+    prevDot.classList.remove("active-dot");
+  }
+
+  function leftSlider(nextImg) {
+    if (currImg <= sliderImgs.length) {
+      nextImg = currImg + 1 < sliderImgs.length ? currImg + 1 : 0;
+    } else {
+      --nextImg;
+    }
+
+    // nextImg = currImg + 1;
+
+    sliderImgs[prevImg].style.animationName = "";
+
+    sliderImgs[nextImg].style.animationName = "leftNext";
+    sliderImgs[currImg].style.animationName = "leftCurr";
+
+    prevImg = currImg;
+    currImg = nextImg;
+
+    let currDot = allDots[currImg];
+    currDot.classList.add("active-dot");
+    let prevDot = allDots[prevImg];
+    prevDot.classList.remove("active-dot");
+  }
+
+  /**
+   * Decides if animate to left or right and highlights clicked dot
+   * @param {number} num - index of clicked dot
+   */
+  function dotClick(num) {
+    if (num == currImg) return false;
+
+    clearTimeout(timeout);
+    clearInterval(intrvl);
+
+    if (num > currImg) animateSlider(num + 1);
+    else animateSlider(num + 1, true);
+
+    intrvl = setInterval(animateSlider, interval);
+  }
+
+  // swiper for top-slider
+
+  let isEnabled = true;
+
+  const swipedetect = (el) => {
+    let surface = el;
+    let startX = 0;
+    let startY = 0;
+    let distX = 0;
+    let distY = 0;
+    let startTime = 0;
+    let elapsedTime = 0;
+
+    let threshold = 150;
+    let restraint = 100;
+    let allowedTime = 300;
+
+    surface.addEventListener(
+      "mousedown",
+      function (e) {
+        startX = e.pageX;
+        startY = e.pageY;
+        startTime = new Date().getTime();
+        e.preventDefault();
+      },
+      false
+    );
+
+    surface.addEventListener(
+      "mouseup",
+      function (e) {
+        distX = e.pageX - startX;
+        distY = e.pageY - startY;
+        elapsedTime = new Date().getTime() - startTime;
+        if (elapsedTime <= allowedTime) {
+          if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+            if (distX > 0) {
+              if (isEnabled) {
+                rightSlider(nextImg);
+              }
+            } else {
+              if (isEnabled) {
+                leftSlider(nextImg);
+              }
+            }
+          }
+        }
+        e.preventDefault();
+      },
+      false
+    );
+
+    surface.addEventListener(
+      "touchstart",
+      function (e) {
+        if (
+          e.target.classList.contains("arrow") ||
+          e.target.classList.contains("control")
+        ) {
+          if (e.target.classList.contains("left")) {
+            if (isEnabled) {
+              // previousItem(currentItem);
+              rightSlider(nextImg);
+            }
+          } else {
+            if (isEnabled) {
+              // nextItem(currentItem);
+              leftSlider(nextImg);
+            }
+          }
+        }
+        var touchobj = e.changedTouches[0];
+        startX = touchobj.pageX;
+        startY = touchobj.pageY;
+        startTime = new Date().getTime();
+        e.preventDefault();
+      },
+      false
+    );
+
+    surface.addEventListener(
+      "touchmove",
+      function (e) {
+        e.preventDefault();
+      },
+      false
+    );
+
+    surface.addEventListener(
+      "touchend",
+      function (e) {
+        var touchobj = e.changedTouches[0];
+        distX = touchobj.pageX - startX;
+        distY = touchobj.pageY - startY;
+        elapsedTime = new Date().getTime() - startTime;
+        if (elapsedTime <= allowedTime) {
+          if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+            if (distX > 0) {
+              if (isEnabled) {
+                // previousItem(currentItem);
+                rightSlider(nextImg);
+              }
+            } else {
+              if (isEnabled) {
+                leftSlider(nextImg);
+              }
+            }
+          }
+        }
+        e.preventDefault();
+      },
+      false
+    );
+  };
+
+  var el = document.querySelector(".top-slider");
+  swipedetect(el);
+}
+var el = document.querySelector(".top-slider");
